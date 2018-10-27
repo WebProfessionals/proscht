185e0adadb05c6165f16dda5856a37a9b701fd6c
Marielle Beusch <mariellebeusch@Marielles-MacBook-Pro.local>
2018-10-27 20:19:40 +0200

    updated joingame

--- src/components/JoinGame.vue
+++ src/components/JoinGame.vue
@@ -1,18 +1,29 @@
-<template>
-  <div>
-    Spiel beitreten
-    <br>
-    <v-text-field
-      id="gameIdInput"
-      label="Game Id"
-      v-model="gameId"
-    ></v-text-field>
-    <br>
-    <v-btn v-on:click="joinGame">beitreten</v-btn>
 
-  </div>
+  <template>
+    <div class="text-xs-center container">
 
-</template>
+      <div>
+        <v-container fluid grid-list-xl>
+          <h2 class="display-3">Wähle deine Figur</h2>
+          <v-btn class="player-button"><img src="@/assets/img/luckyluke.svg" alt="Lucky Luke"></v-btn>
+          <v-btn class="player-button"><img src="@/assets/img/supermario.svg" alt="Lucky Luke"></v-btn>
+          <v-btn class="player-button"><img src="@/assets/img/lukeskywalker.svg" alt="Lucky Luke"></v-btn>
+          <v-btn class="player-button"><img src="@/assets/img/peach.svg" alt="Lucky Luke"></v-btn>
+        </v-container>
+        <br>
+        <v-text-field
+                id="gameIdInput"
+                label="Game Id"
+                v-model="gameId"
+        ></v-text-field>
+        <br>
+        <v-btn class="join-btn" v-on:click="joinGame">Beitreten</v-btn>
+
+      </div>
+
+    </div>
+  </template>
+
 <script>
   import { mapState } from 'vuex'
   const fb = require('../helpers/firebaseConfig')
@@ -44,3 +55,40 @@
     }
   }
 </script>
+
+  <style scoped>
+
+    h2.display-3 {
+      margin-bottom: 40px;
+    }
+
+    .player-button {
+      width: 12em;
+      height: auto;
+      padding: 40px;
+    }
+
+    .theme--light.v-btn:not(.v-btn--icon):not(.v-btn--flat) {
+      background-color: transparent;
+    }
+
+    .v-btn:not(.v-btn--depressed):not(.v-btn--flat) {
+      box-shadow: 0 0 0;
+    }
+
+    .v-text-field {
+      visibility: hidden;
+    }
+
+    .v-btn.join-btn {
+      background-color: #000!important;
+      color: white;
+      width: 15em;
+      height: 50px;
+    }
+
+    .v-btn__content {
+      color: #fff!important;
+    }
+
+  </style>
