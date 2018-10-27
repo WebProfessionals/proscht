af6406ee5d34cc8dc453f3abb41f16f06f6c18e5
Rolf Eggenberger <rolf@kunstwort.com>
2018-10-27 17:33:54 +0200

    import example

---
+++ src/components/Import.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+  import { mapState } from 'vuex'
+  const fb = require('../helpers/firebaseConfig')
+  export default {
+    name: 'GameBoard',
+    data () {
+      return {
+        questions: [
+          {
+            question: 'Frage 1',
+            answer1: 'Antwort 1',
+            answer2: 'Antwort 2',
+            answer3: 'Antwort 3',
+            answer4: 'Antwort 4'
+          },
+          {
+            question: 'Frage 2',
+            answer1: 'Antwort 1',
+            answer2: 'Antwort 2',
+            answer3: 'Antwort 3',
+            answer4: 'Antwort 4'
+          }
+        ]
+      }
+    }
+  }
+</script>
