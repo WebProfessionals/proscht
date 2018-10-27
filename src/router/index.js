93288bf9d9ac02985f3fde9c6b0981aa7981e316
joelrhenisch <joel.rhenisch@gmail.com>
2018-10-27 11:56:12 +0200

    add home

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Beispiel from '@/components/Beispiel'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -16,6 +17,12 @@ export default new Router({
       path: '/beispiel',
       name: 'Beispiel',
       component: Beispiel
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
+
   ]
 })
