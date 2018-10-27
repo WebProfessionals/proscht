153241f3734268d17632006839b6aea7e181cfa2
kennydies <claudio.h@ilnet.ch>
2018-10-27 12:59:01 +0200

    ShareLink component

--- src/components/ShareLink.vue
+++ src/components/ShareLink.vue
@@ -5,8 +5,8 @@
             </v-btn>
             <h5 class="lbl text-xs-center">invite players with this link:</h5>
             <div class="linkBox">
-                    <code> http://localhost:8080/gameID/928347 {{ gameLink }} </code>
-                    <v-btn small class="btnCopy text-xs-center" v-on:click="copyGameLink">copy</v-btn>
+                <code> http://localhost:8080/gameID/928347 {{ gameLink }} </code>
+                <v-btn small class="btnCopy text-xs-center" v-on:click="copyGameLink">copy</v-btn>
             </div>
         </div>
 </template>
@@ -19,16 +19,9 @@
     .btnCopy{
         margin: 15px auto;
     }
-    .lbl{
-        padding-top: 25vh;
-    }
     .linkBox{
         display: flex;
         justify-content: center;
         flex-direction: column;
     }
-    code{
-        margin: 0 auto;
-    }
-
 </style>
