8db2ef737295360d773037bb8055e92859c44c28
Marielle Beusch <mariellebeusch@Marielles-MacBook-Pro.local>
2018-10-27 12:55:25 +0200

    added Players

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Beispiel from '@/components/Beispiel'
+import Players from '@/components/Players'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
       path: '/beispiel',
       name: 'Beispiel',
       component: Beispiel
+    },
+    {
+      path: '/players',
+      name: 'Players',
+      component: Players
     }
   ]
 })
