dd3759f995e68c05193b07dc93dd0e3d25c1ec87
Liviak5 <livia.kobler@bluewin.ch>
2018-10-27 12:36:14 +0200

    gameboard dummy

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Beispiel from '@/components/Beispiel'
+import GameBoard from '@/components/GameBoard'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/beispiel',
       name: 'Beispiel',
       component: Beispiel
+    },
+    {
+      path: '/gameboard',
+      name: 'GameBoard',
+      component: GameBoard
     }
   ]
 })
